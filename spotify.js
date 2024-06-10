5cb28aac1a91e3d4ffe7e4243d51357c85bf59e2
Riddhilalla <riddhilalla16@gmail.com>
2024-06-11 04:31:16 +0530

    done

--- spotify.js
+++ spotify.js
@@ -1,4 +1,16 @@
 console.log('JS starts');
+let currentsong = new Audio();
+let songs;
+let track;
+function sectomin(seconds){
+    const minutes = Math.floor(seconds/60);
+    const remaining = Math.floor(seconds % 60);
+
+    const formatmin = String(minutes).padStart(2,'0');
+    const formatsec = String(remaining).padStart(2,'0');
+
+    return `${formatmin}:${formatsec}`;
+}
 
 async function getsongs() {
    
@@ -29,8 +41,18 @@ async function getsongs() {
         return songs;
     
 }
+
+const playmusic=(track) =>{
+   // let audio = new Audio("/songs/" + track)
+   currentsong.src= "/songs/" + track
+   currentsong.play()
+    play.src ="pause.svg"
+    document.querySelector(".songInfo").innerHTML=track
+    document.querySelector(".songtime").innerHTML="00:00 / 00:00"
+}
 async function main(){
-    let songs = await getsongs();
+   
+    songs = await getsongs();
     console.log(songs);
 
     let songul = document.querySelector(".songlist").getElementsByTagName("ul")[0]
@@ -55,5 +77,84 @@ async function main(){
         let duration = audio.duration;
         console.log(audio.duration,audio.currentSrc,audio.currentTime)
     })
+
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".info").children[1].innerHTML)
+            playmusic(e.querySelector(".info").children[1].innerHTML.trim())
+        })
+        
+    })
+    //play next and prev
+    play.addEventListener("click",()=>{
+        if(currentsong.paused){
+            currentsong.play()
+            play.src ="pause.svg"
+        }
+        else{
+            currentsong.pause()
+            play.src="playpause.svg"
+        }
+    })
+    //select song
+    let previousSong = null;
+
+document.querySelectorAll(".songlist > ul > li").forEach(song => {
+    song.addEventListener("click", (e) => {
+        if (previousSong) {
+            previousSong.style.backgroundColor = ""; 
+        }
+        e.currentTarget.style.backgroundColor = "#555";
+        previousSong = e.currentTarget;
+    });
+});
+
+    //timeupdate
+    currentsong.addEventListener("timeupdate",()=>{
+        console.log(currentsong.currentTime,currentsong.duration);
+        document.querySelector(".songtime").innerHTML = `${sectomin(currentsong.currentTime)} / ${sectomin(currentsong.duration)}`;
+        document.querySelector(".circle").style.left = ( currentsong.currentTime / currentsong.duration)*100 + "%"
+    })
+
+    //aad seekbar func
+    
+    document.querySelector(".playtrack").addEventListener("click",e=>{
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100
+        document.querySelector(".circle").style.left = percent + "%";
+        currentsong.currentTime = (currentsong.duration * percent )/ 100;
+    })
+
+    //prev and next
+    prev.addEventListener("click", () => {
+        const currentSongName = currentsong.src.split("/").slice(-1)[0];
+        const currentIndex = songs.findIndex(song => song.endsWith(currentSongName));
+        let previousIndex;
+        if (currentIndex !== -1 && currentIndex > 0) {
+            previousIndex = currentIndex - 1;
+        } else {
+            
+            previousIndex = songs.length - 1;
+        }
+        const previousSongName = songs[previousIndex];
+        playmusic(decodeURIComponent(previousSongName));
+    });
+    
+    next.addEventListener("click", () => {
+        const currentSongName = currentsong.src.split("/").slice(-1)[0];
+        const currentIndex = songs.findIndex(song => song.endsWith(currentSongName));
+       let nextIndex;
+       if (currentIndex !== -1 && currentIndex + 1 < songs.length) {
+           nextIndex = currentIndex + 1;
+       } else {
+           
+           nextIndex = 0;
+       }
+       const nextSongName = songs[nextIndex];
+       playmusic(decodeURIComponent(nextSongName));
+    });
+    
+    
+    
+    
 }
 main()
